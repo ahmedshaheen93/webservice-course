4dbd74e42aa56b1001048cf2f6e1721e5b82128e
ahmedshaheen <ahmedshaheen676@yahoo.com>
2020-03-23 11:56:39 +0200

    day 1 Calculator with servelt

---
+++ day1-calculatorServelt/src/main/java/com/shaheen/Calculator.java
@@ -0,0 +1,26 @@
+package com.shaheen;
+
+import javax.jws.WebService;
+
+@WebService
+public class Calculator {
+    public float addNumbers(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public float subNumbers(Float num1, Float num2) {
+        return num1 - num2;
+    }
+
+    public float divNumbers(Float num1, Float num2) {
+        if (num2 == 0) {
+            return Float.NaN;
+        }
+        return num1 / num2;
+    }
+
+    public float mulNumbers(Float num1, Float num2) {
+        return num1 * num2;
+    }
+
+}
